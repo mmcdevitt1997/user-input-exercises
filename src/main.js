287e1dc8f0a355c94e0ab3cef3b760cd2125c7cb
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-06-17 15:35:58 -0500

    done you can add data

--- src/main.js
+++ src/main.js
@@ -5,3 +5,24 @@ const legoCreation = {
     shape: "Animal",
     creation: "Black Cow"
 }
+// Some starter code
+document.querySelector(".btn").addEventListener("click", event => {
+    const creator = document.querySelector("#lego__creator").value
+    const color = document.querySelector("#lego-color").value
+
+    // Once you have collected all the values, build your data structure
+    const legoToSave = {
+        
+        creator: creator,
+        color: color
+    }
+    
+    fetch("http://localhost:8088/lego",{ 
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(legoToSave)
+})
+
+})
